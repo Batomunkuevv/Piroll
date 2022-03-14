9e2001edb1d3dec919f81401a632cc0dfe67bd91
Kirill <batomunkuevkirill@yandex.ru>
2022-03-14 23:06:32 +0700

    Change bg menu and change bg header

--- js/script.js
+++ js/script.js
@@ -12,14 +12,19 @@ function ibg(){
 
 let header__burger = document.querySelector('.header__burger');
 let header_menu = document.querySelector('.header__menu');
+let header = document.querySelector('.header');
+let body = document.querySelector('body');
 let header__list = document.querySelector('.header__list');
-
+	
 header__burger.onclick = function(){
 	header__burger.classList.toggle('active');
 	header_menu.classList.toggle('active');
-	header__body.classList.toggle('lock');
+	header.classList.toggle('active');
+	body.classList.toggle('lock');
 }
 	
 header__list.onclick = function () {
 	header__list.classList.remove('active');
+	header.classList.remove('active');
+	body.classList.toggle('lock');
 }
