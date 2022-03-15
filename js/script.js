a092b097aff2da43fb024a493b9fbd4a52ac99f8
Kirill <batomunkuevkirill@yandex.ru>
2022-03-15 18:28:17 +0700

    Adaptive  skills and about

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
-function ibg(){
-
-	let ibg=document.querySelectorAll(".ibg");
-	for (var i = 0; i < ibg.length; i++) {
-	if(ibg[i].querySelector('img')){
-	ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
-	}
-	}
-	}
-	
-	ibg();
-
-let header__burger = document.querySelector('.header__burger');
-let header_menu = document.querySelector('.header__menu');
-let header = document.querySelector('.header');
-let body = document.querySelector('body');
-let header__list = document.querySelector('.header__list');
-	
-header__burger.onclick = function(){
-	header__burger.classList.toggle('active');
-	header_menu.classList.toggle('active');
-	header.classList.toggle('active');
-	body.classList.toggle('lock');
+function ibg() {
+  let ibg = document.querySelectorAll(".ibg");
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector("img")) {
+      ibg[i].style.backgroundImage =
+        "url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
+    }
+  }
 }
-	
+
+ibg();
+
+let header__burger = document.querySelector(".header__burger");
+let header_menu = document.querySelector(".header__menu");
+let header = document.querySelector(".header");
+let body = document.querySelector("body");
+let header__list = document.querySelector(".header__list");
+
+header__burger.onclick = function () {
+  header__burger.classList.toggle("active");
+  header_menu.classList.toggle("active");
+  header.classList.toggle("active");
+  body.classList.toggle("lock");
+};
+
 header__list.onclick = function () {
-	header__list.classList.remove('active');
-	header.classList.remove('active');
-	body.classList.toggle('lock');
-}
+  header__list.classList.remove("active");
+  header.classList.remove("active");
+  body.classList.toggle("lock");
+};
